7335907735733f35cf6ebcaf21d15aed4f5f7b0d
Prodeon <prodeon21@gmail.com>
2026-04-19 08:08:02 +0300

    make migrations and migrate

--- teaching/models.py
+++ teaching/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.db.models import SET_NULL
+from django.db.models import SET_NULL, CASCADE
 
 
 # Create your models here.
 
 class Course(models.Model):
-    title = models.CharField(100, verbose_name="Название")
+    title = models.CharField(max_length=100, verbose_name="Название")
     preview = models.ImageField(verbose_name="Превью(картинка)")
     description = models.TextField(verbose_name="Описание")
 
@@ -17,11 +17,11 @@ class Course(models.Model):
         verbose_name_plural = "Курсы"
 
 class Lesson(models.Model):
-    title = models.CharField(100, verbose_name="Название")
+    title = models.CharField(max_length=100, verbose_name="Название")
     preview = models.ImageField(verbose_name="Превью(картинка)")
     description = models.TextField(verbose_name="Описание")
     video_url = models.URLField(verbose_name="Ссылка на видео")
-    course = models.ForeignKey(to=Course, on_delete=SET_NULL, verbose_name="курс")
+    course = models.ForeignKey(to=Course, on_delete=CASCADE, verbose_name="курс")
 
     def __str__(self):
         return self.title
