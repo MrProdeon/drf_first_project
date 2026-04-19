ef1354fab84a0b6bba0b71458845882d6121d6c0
Prodeon <prodeon21@gmail.com>
2026-04-19 07:58:09 +0300

    add model Lesson

--- teaching/models.py
+++ teaching/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import SET_NULL
+
 
 # Create your models here.
 
@@ -14,3 +16,16 @@ class Course(models.Model):
         verbose_name = "Курс"
         verbose_name_plural = "Курсы"
 
+class Lesson(models.Model):
+    title = models.CharField(100, verbose_name="Название")
+    preview = models.ImageField(verbose_name="Превью(картинка)")
+    description = models.TextField(verbose_name="Описание")
+    video_url = models.URLField(verbose_name="Ссылка на видео")
+    course = models.ForeignKey(to=Course, on_delete=SET_NULL, verbose_name="курс")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
