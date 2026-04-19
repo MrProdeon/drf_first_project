496b08310f71b180dbec391638d4f6f69c2f63f7
Prodeon <prodeon21@gmail.com>
2026-04-19 07:54:42 +0300

    add model Course

--- teaching/models.py
+++ teaching/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(100, verbose_name="Название")
+    preview = models.ImageField(verbose_name="Превью(картинка)")
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
