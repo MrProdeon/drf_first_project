3b85b2453e029c3fbc4c4d6d0c134fe7540ae164
Prodeon <prodeon21@gmail.com>
2026-04-20 11:41:31 +0300

    add view and urls for lessons

--- teaching/urls.py
+++ teaching/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from teaching.views import CourseViewSet
+from teaching.views import CourseViewSet, LessonListCreateApiView, LessonRetriveUpdateDestroyApiView
 
 from teaching.apps import TeachingConfig
 
@@ -11,5 +11,7 @@ router = SimpleRouter()
 router.register(r"courses", CourseViewSet, basename="course")
 
 urlpatterns = [
+    path("lessons/", LessonListCreateApiView.as_view(), name="lessons-list"),
+    path("lessons/<int:pk>/", LessonRetriveUpdateDestroyApiView.as_view(), name="lesson-detail")
 
 ] + router.urls
