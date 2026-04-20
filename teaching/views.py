48bc6ab3bccf8968a94b8d369db0bee5e8edeb7b
Prodeon <prodeon21@gmail.com>
2026-04-20 11:23:09 +0300

    create view set for courses

--- teaching/views.py
+++ teaching/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from teaching.models import Course, Lesson
+from teaching.serializers import CourseSerializer, LessonSerializer
+
 
 # Create your views here.
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
