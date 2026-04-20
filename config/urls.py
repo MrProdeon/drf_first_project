3608b9d7185b71f1d661124db83aca442cf94395
Prodeon <prodeon21@gmail.com>
2026-04-20 12:03:10 +0300

    add serializer, urls and view for CRUD in CustomUser

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("teaching.urls", namespace="teaching")),
+    path("users/", include("users.urls", namespace="users"))
 
 ]
