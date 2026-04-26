b1fb6e49e4ff3c9c9ef2d639c2eb9c3d8e394250
Prodeon <prodeon21@gmail.com>
2026-04-26 13:51:57 +0300

    add ViewSet for model Payments

--- users/views.py
+++ users/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
-from users.models import CustomUser
-from users.serializers import UserSerializer
-
-
-# Create your views here.
+from users.models import CustomUser, Payments
+from users.serializers import UserSerializer, PaymentSerializer
 
 class UserViewSet(ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+class PaymentViewSet(ModelViewSet):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    ordering_fields = ["payment_date"]
+    filterset_fields = ["course", "lesson", "payment_method"]
