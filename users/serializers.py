0d44b26edd83fa1b8cc6f42c549bc93cb9584afd
Prodeon <prodeon21@gmail.com>
2026-04-26 13:51:38 +0300

    add PaymentSerializer for model Payments

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from users.models import CustomUser
+from users.models import CustomUser, Payments
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
+        fields = "__all__"
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payments
         fields = "__all__"
