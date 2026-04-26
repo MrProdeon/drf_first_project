714de391eca168bfa79da7310c0c0ee6964b4213
Prodeon <prodeon21@gmail.com>
2026-04-26 13:27:55 +0300

    add settings for django filters

--- config/settings.py
+++ config/settings.py
@@ -44,9 +44,16 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "users",
-    "teaching"
+    "teaching",
+    "django_filters"
 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
