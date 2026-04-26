612402f2568f78687c54a3f3f6b630e50085955f
Prodeon <prodeon21@gmail.com>
2026-04-26 13:52:24 +0300

    add urls in router for model Payment

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from users.views import UserViewSet
+from users.views import UserViewSet, PaymentViewSet
 
 from users.apps import UsersConfig
 
@@ -9,6 +9,7 @@ app_name = UsersConfig.name
 
 router = SimpleRouter()
 router.register(r"users", UserViewSet, basename="users")
+router.register(r"payments", PaymentViewSet, basename="payments")
 
 urlpatterns = [
 
