82a1c9adc78c73ecc8a8d03ed1fd93167ddfb8a3
Prodeon <prodeon21@gmail.com>
2026-04-30 14:58:17 +0300

    add permission IsNotModerator

--- users/permissions.py
+++ users/permissions.py
@@ -4,3 +4,7 @@ from rest_framework.permissions import BasePermission
 class IsModerator(BasePermission):
     def has_permission(self, request, view):
         return request.user.groups.filter(name="moderators").exists()
+
+class IsNotModerator(BasePermission):
+    def has_permission(self, request, view):
+        return not request.user.groups.filter(name="moderators").exists()
