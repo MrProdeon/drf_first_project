bba01782a6efc05722afdd858c2ae9a1b67fe09b
Prodeon <prodeon21@gmail.com>
2026-05-01 07:19:29 +0300

    add owner in teaching models

--- teaching/models.py
+++ teaching/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.db.models import SET_NULL, CASCADE
+from config import settings
+
+
 
 
 # Create your models here.
@@ -8,6 +11,7 @@ class Course(models.Model):
     title = models.CharField(max_length=100, verbose_name="Название")
     preview = models.ImageField(verbose_name="Превью(картинка)", null=True, blank=True)
     description = models.TextField(verbose_name="Описание")
+    owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=CASCADE, null=True, blank=True, verbose_name="Владелец")
 
     def __str__(self):
         return self.title
@@ -22,6 +26,7 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name="Описание")
     video_url = models.URLField(verbose_name="Ссылка на видео")
     course = models.ForeignKey(to=Course, on_delete=CASCADE, verbose_name="курс", related_name="lessons")
+    owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=CASCADE, null=True, blank=True, verbose_name="Владелец")
 
     def __str__(self):
         return self.title
