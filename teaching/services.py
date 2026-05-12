3600cff3c45b3ec3dee5f9a9841eadf962bff6f9
Prodeon <prodeon21@gmail.com>
2026-05-12 19:11:02 +0300

    add functions for create product, price and session with stripe

---
+++ teaching/services.py
@@ -0,0 +1,36 @@
+import stripe
+from config import settings
+
+stripe.api_key = settings.STRIPE_API_KEY
+
+def create_stripe_product(course):
+    """Создает продукт в страйпе"""
+    product = stripe.Product.create(
+        name=course.title,
+        description=course.description if course.description else None,
+        metadata={
+            'django_course_id': course.id,
+        }
+    )
+    return product.id
+
+
+def create_stripe_price(product_id, amount_in_cents, currency="rub"):
+    """Создает цену в страйпе"""
+
+    price = stripe.Price.create(
+        product=product_id,
+        unit_amount=amount_in_cents * 100,
+        currency=currency,
+    )
+    return price.id
+
+def create_stripe_session(price_id):
+    """Создает сессию в страйпе"""
+    session = stripe.checkout.Session.create(
+        success_url="http://127.0.0.1:8000/",
+        cancel_url="http://127.0.0.1:8000/",
+        line_items=[{"price": price_id, "quantity": 1}],
+        mode="payment",
+    )
+    return session.id, session.url
