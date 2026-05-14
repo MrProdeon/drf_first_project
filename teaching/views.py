092f96e954b0a46c8a8565fd8ebccd1e5db7e0b1
Prodeon <prodeon21@gmail.com>
2026-05-14 06:54:44 +0300

    add swagger_auto_schema in lessons views

--- teaching/views.py
+++ teaching/views.py
@@ -13,6 +13,10 @@ from teaching.paginators import ProjectPagination
 from users.models import CustomUser
 from users.permissions import IsModerator, IsNotModerator, IsOwner
 
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from utils.common_errors import common_errors
+
 
 # Create your views here.
 class CourseViewSet(ModelViewSet):
@@ -67,6 +71,74 @@ class LessonListCreateApiView(ListCreateAPIView):
         lesson.owner = self.request.user
         lesson.save()
 
+    @swagger_auto_schema(
+        operation_description="Получить список уроков (с учетом прав доступа)",
+        responses={
+            status.HTTP_200_OK: openapi.Response(
+                description='Успешный ответ со списком уроков',
+                schema=LessonSerializer,
+                examples={
+                    'application/json': {
+                        'count': 10,
+                        'next': 'http://...',
+                        'previous': None,
+                        'results': [
+                            {
+                                'id': 1,
+                                'title': 'Введение в Python',
+                                'owner': 5,
+                                'created_at': '2024-01-01T12:00:00Z'
+                            }
+                        ]
+                    }
+                }
+            ),
+            **common_errors
+        }
+    )
+    def get(self, request, *args, **kwargs):
+
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Создать новый урок",
+        request_body=LessonSerializer,
+        responses={
+            status.HTTP_201_CREATED: openapi.Response(
+                description='Урок успешно создан',
+                examples={
+                    'application/json': {
+                        'id': 1,
+                        'title': 'Новый урок',
+                        'owner': 5,
+                        'created_at': '2024-01-01T12:00:00Z'
+                    }
+                }
+            ),
+            status.HTTP_400_BAD_REQUEST: openapi.Response(
+                description="Неверно составлен запрос",
+                examples={
+                    "application/json": {
+
+                        "video_url": [
+                            "This field is required."
+                        ],
+                        "title": [
+                            "This field is required."
+                        ],
+                        "description": [
+                            "This field is required."
+                        ]
+
+                    }
+                }
+            ),
+            **common_errors
+        })
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
 class LessonRetriveUpdateDestroyApiView(RetrieveUpdateDestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
@@ -85,6 +157,80 @@ class LessonRetriveUpdateDestroyApiView(RetrieveUpdateDestroyAPIView):
         else:
             return [IsAuthenticated()]
 
+    @swagger_auto_schema(
+        operation_description="Получить урок",
+        responses={
+            status.HTTP_200_OK: openapi.Response(
+                description='Урок получен',
+                examples={
+                    'application/json': {
+                        "id": 0,
+                        "video_url": "string",
+                        "title": "string",
+                        "preview": "string",
+                        "description": "string",
+                        "course": 0,
+                        "owner": 0
+                    }
+                }
+            ),
+        **common_errors})
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Полностью изменить урок",
+        responses={
+            status.HTTP_201_CREATED: openapi.Response(
+                description='Урок изменен',
+                examples={
+                    'application/json': {
+                        "id": 0,
+                        "video_url": "string",
+                        "title": "string",
+                        "preview": "string",
+                        "description": "string",
+                        "course": 0,
+                        "owner": 0
+                    }
+                }
+            ),
+            **common_errors})
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Частично изменить урок",
+        responses={
+            status.HTTP_201_CREATED: openapi.Response(
+                description='Урок изменен',
+                examples={
+                    'application/json': {
+                        "id": 0,
+                        "video_url": "string",
+                        "title": "string",
+                        "preview": "string",
+                        "description": "string",
+                        "course": 0,
+                        "owner": 0
+                    }
+                }
+            ),
+            **common_errors})
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Удалить урок",
+        responses={
+            status.HTTP_204_NO_CONTENT: openapi.Response(
+                description='Урок удален',
+            ),
+            **common_errors})
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
+
+
 class SubscriptionsAPIView(APIView):
     def post(self, *args, **kwargs):
 
@@ -102,6 +248,4 @@ class SubscriptionsAPIView(APIView):
             Subscription.objects.create(course=course_item, user=user)
             message = f"Подписка пользователя {user.email} на курс {course_item.title} добавлена"
 
-        return Response({"message" : message}, status=status.HTTP_201_CREATED)
-
-
+        return Response({"message": message}, status=status.HTTP_201_CREATED)
